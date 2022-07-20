0a86c9884b3a7d74ff307c98b0d7083c7db906ce
Enriko <enrico@DESKTOP-U1C1S9D.localdomain>
2022-07-20 10:46:29 +0200

    Add POST planet route

--- src/server.test.ts
+++ src/server.test.ts
@@ -37,3 +37,20 @@ test("GET /planets", async () => {
 
     expect(response.body).toEqual(planets);
 });
+
+test("POST /planets", async () => {
+    const planet = {
+        id: 1,
+        name: "Mercury",
+        diameter: 1234,
+        moons: 12,
+    };
+
+    const response = await request
+        .post("/planets")
+        .send(planet)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toEqual(planet);
+});
